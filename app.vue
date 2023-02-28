53d019c76c4161639c5c3110a904fbd06d9f7525
Igor Melnikov <igor2354@yandex.ru>
2023-02-28 10:13:21 +0300

    test api laravel nuxt 3

--- app.vue
+++ app.vue
@@ -8,9 +8,11 @@
 
 <script setup>
 if (useCookie("tokenAccess").value != undefined) {
-	await $fetch("http://api.wesma.ru/api/auth/refresh", {
+	await $fetch("http://api.wesma.local/api/auth/refresh", {
 		method: "POST",
-		authorization: `Bearer ${useCookie("tokenAccess").value}`,
+		headers: {
+			authorization: `Bearer ${useCookie("tokenAccess").value}`,
+		},
 	})
 		.then((data) => {
 			console.log(data);
